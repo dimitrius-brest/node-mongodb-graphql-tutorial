56bf34376200536ba9c6ae0d53e12993b2833694
dimitrius-brest <d_der@mail.ru>
2019-12-17 17:48:18 +0300

    Lesson #5

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const graphQlHttp = require('express-graphql');
 const { buildSchema } = require('graphql');
 const mongoose = require('mongoose');
 
-const app = express();
+const Event = require('./models/event');
 
-const events = [];
+const app = express();
 
 app.use(bodyParser.json());
 
@@ -44,25 +44,42 @@ app.use(
     `),
     rootValue: {
         events: () => {
-            return events;
+            return Event.find()
+                .then(events => {
+                    return events.map(event => {
+                        //return { ...event._doc };
+                        //return { ...event._doc, id: event._doc._id.toString() };
+                        return { ...event._doc, id: event.id };
+                    });
+                })
+                .catch(err => {
+                    throw err;
+                });
         },
         createEvent: (args) => {
-            const event = {
-                _id: Math.random().toString(),
+            const event = new Event({
                 title: args.eventInput.title,
                 description: args.eventInput.description,
                 price: +args.eventInput.price,
-                date: args.eventInput.date
-            };
-            events.push(event);
-            return event;
+                date: new Date(args.eventInput.date)
+            });
+            return event
+                .save()
+                .then(result => {
+                    console.log(result);
+                    //return { ...result._doc };
+                    return { ...result._doc, _id: result._doc._id.toString() };
+                }).catch(err => {
+                    console.log(err);
+                    throw err;
+                });
         }
     },
     graphiql: true
   })
 );
 
-mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-dlihq.mongodb.net/test?retryWrites=true&w=majority`)
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-dlihq.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`)
     .then(() => {
         app.listen(3000);
     }).catch(err => {
