e3eda8b29e33ef59557de9a886cd4fdd8029ee54
dimitrius-brest <d_der@mail.ru>
2020-01-20 17:41:12 +0300

    Lesson #10

--- graphql/resolvers/events.js
+++ graphql/resolvers/events.js
@@ -1,4 +1,5 @@
 const Event = require('../../models/event');
+const User = require('../../models/user');
 
 const { transformEvent } = require('./merge');
 
@@ -13,19 +14,22 @@ module.exports = {
             throw err;
         }
     },
-    createEvent: async args => {
+    createEvent: async (args, req) => {
+        if (!req.isAuth) {
+            throw new Error('Unauthenticated!');
+        }
         const event = new Event({
             title: args.eventInput.title,
             description: args.eventInput.description,
             price: +args.eventInput.price,
             date: new Date(args.eventInput.date),
-            creator: '5e1c670c1e400b2504bcfb9d'
+            creator: req.userId
         });
         let createdEvent;
         try {
             const result = await event.save();
             createdEvent = transformEvent(result);
-            const creator = await User.findById('5e1c670c1e400b2504bcfb9d');
+            const creator = await User.findById(req.userId);
 
             if (!creator) {
                 throw new Error('User not found.');
