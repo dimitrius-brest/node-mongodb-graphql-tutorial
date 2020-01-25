4d2979c658075fe62a8116794355af69fdd0170a
Dmitry Derevitsky <d_der@mail.ru>
2020-01-25 20:46:07 +0300

    Lesson #17

--- frontend/src/components/Modal/Modal.js
+++ frontend/src/components/Modal/Modal.js
@@ -18,7 +18,7 @@ const modal = props => (
             )}
             {props.canConfirm && (
                 <button className="btn" onClick={props.onConfirm}>
-                    Confirm
+                    {props.confirmText}
                 </button>
             )}
         </section>
