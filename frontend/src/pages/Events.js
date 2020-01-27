9db925ea04237803a977a217ef065064f1feb6c2
Dmitry Derevitsky <d_der@mail.ru>
2020-01-27 22:04:54 +0300

    Lesson #18

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -14,6 +14,7 @@ class EventsPage extends Component {
         isLoading: false,
         selectedEvent: null
     };
+    isActive = true;
 
     static contextType = AuthContext;
 
@@ -127,8 +128,6 @@ class EventsPage extends Component {
             `
         };
 
-        const token = this.context.token;
-
         fetch('http://localhost:8000/graphql', {
             method: 'POST',
             body: JSON.stringify(requestBody),
@@ -144,11 +143,15 @@ class EventsPage extends Component {
             })
             .then(resData => {
                 const events = resData.data.events;
-                this.setState({ events: events, isLoading: false });
+                if (this.isActive) {
+                    this.setState({ events: events, isLoading: false });
+                }
             })
             .catch(err => {
                 console.log(err);
-                this.setState({ isLoading: false });
+                if (this.isActive) {
+                    this.setState({ isLoading: false });
+                }
             });
     }
 
@@ -157,9 +160,51 @@ class EventsPage extends Component {
             const selectedEvent = prevState.events.find(e => e._id === eventId);
             return {selectedEvent: selectedEvent};
         });
-    }
+    };
 
-    bookEventHandler = () => {}
+    bookEventHandler = () => {
+        if (!this.context.token) {
+            this.setState({selectedEvent: null});
+            return;
+        }
+        const requestBody = {
+            query: `
+              mutation {
+                bookEvent(eventId: "${this.state.selectedEvent._id}") {
+                  _id
+                  createdAt
+                  updatedAt
+                }
+              }
+            `
+        };
+
+        fetch('http://localhost:8000/graphql', {
+            method: 'POST',
+            body: JSON.stringify(requestBody),
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + this.context.token
+            }
+        })
+            .then(res => {
+                if (res.status !== 200 && res.status !== 201) {
+                    throw new Error('Failed!');
+                }
+                return res.json();
+            })
+            .then(resData => {
+                console.log(resData);
+                this.setState({selectedEvent: null});
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    };
+
+    componentWillUnmount() {
+        this.isActive = false;
+    }
 
     render() {
         return (
@@ -201,7 +246,7 @@ class EventsPage extends Component {
                         canConfirm
                         onCancel={this.modalCancelHandler}
                         onConfirm={this.bookEventHandler}
-                        confirmText="Book"
+                        confirmText={this.context.token ? 'Book' : 'Confirm'}
                     >
                         <h1>{this.state.selectedEvent.title}</h1>
                         <h2>
